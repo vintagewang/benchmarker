4d27ff4e762fd2471d473b38e0af6fb527f8e34b
Wangxr <vintage.wang@gmail.com>
2012-08-17 15:03:17 +0800

    ok

--- code/run.sh
+++ code/run.sh
@@ -1,1 +1,5 @@
 nohup ./benchmarker 10.235.170.23:8123 taobao1 1024 1024 512 &
+nohup ./benchmarker 10.235.170.23:8123 taobao1 1024 1024 512 &
+nohup ./benchmarker 10.235.170.23:8123 taobao1 1024 1024 512 &
+nohup ./benchmarker 10.235.170.23:8123 taobao1 1024 1024 512 &
+nohup ./benchmarker 10.235.170.23:8123 taobao1 1024 1024 512 &
